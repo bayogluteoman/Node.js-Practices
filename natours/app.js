cb795b857f5cdc7e9ef5431c997249db019c3f6b
bayogluteoman <bayogluteoman@gmail.com>
2021-12-15 01:57:31 +0300

    created simple middleware

--- natours/app.js
+++ natours/app.js
@@ -6,13 +6,25 @@ const app = express();
 //middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware :)');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,

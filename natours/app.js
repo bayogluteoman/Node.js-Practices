971462a752c2f815949a2c2080c93a3a1faf4ca7
bayogluteoman <bayogluteoman@gmail.com>
2021-12-17 01:46:34 +0300

    set environment variables

--- natours/app.js
+++ natours/app.js
@@ -7,9 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1)MIDDLEWARES
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware :)');

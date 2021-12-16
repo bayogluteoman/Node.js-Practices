49211ec628c401e4942a1737484ad843303fb45f
bayogluteoman <bayogluteoman@gmail.com>
2021-12-16 22:56:47 +0300

    chaine multiple middleware functions

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -4,17 +4,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkID = (req,res,next,val)=>{
-    console.log(`Tour id is: ${val}`);
-    
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-          status: 'fail',
-          message: 'Invalid id',
-        });
-      }
-      next();
-}
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -36,8 +46,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
-
- 
 
   res.status(200).json({
     status: 'success',
@@ -67,7 +75,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -77,8 +84,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-
-
   res.status(204).json({
     status: 'success',
     data: null,

fc69bb187524cce5f6ad72929597fc9f4751cb2a
bayogluteoman <bayogluteoman@gmail.com>
2021-12-17 23:31:16 +0300

    install&set eslint and prettier

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,8 @@
-const dotenv= require('dotenv');
-dotenv.config({ path:'./config.env' });
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

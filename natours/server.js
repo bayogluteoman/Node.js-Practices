d22a3584ac4ec7772350e40b9689dc25bf2c9766
bayogluteoman <bayogluteoman@gmail.com>
2021-12-23 03:02:46 +0300

    connect database with Express App

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection is successfull'));
 
 const app = require('./app');
 

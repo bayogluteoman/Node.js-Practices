b84f3f99de49b0ec3593007d8fa551fbaf594f02
bayogluteoman <bayogluteoman@gmail.com>
2021-12-29 02:48:36 +0300

    update(!) documents with mongoose

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
-const userController = require('./../controllers/userController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
-router.route(':id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;

dc38242c356ad8cd3217e8d35135259ea20c99dc
bayogluteoman <bayogluteoman@gmail.com>
2021-12-28 23:17:54 +0300

    create documents using mongoose

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
 // exports.checkID = (req, res, next, val) => {
 //   console.log(`Tour id is: ${val}`);
@@ -11,16 +11,6 @@ const Tour = require('./../models/tourModel');
 //   }
 //   next();
 // };
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -51,13 +41,25 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    //   const newTour = new Tour();
+    //   newTour.save();
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
   .route('/:id')
